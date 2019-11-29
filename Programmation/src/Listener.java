145518843b15e14370a363c1cf0692ddb0c14c5a
rizziemma <rizziemma@gmail.com>
2019-11-29 17:49:44 +0100

    Debut gestionnaire reseaux et connexiosn tcp

--- Programmation/src/Listener.java
+++ Programmation/src/Listener.java
@@ -1,1 +1,37 @@
 package src;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Listener extends Thread {
+	private ServerSocket server;
+	private ArrayList<GestionnaireConversation> convs;
+	
+	public Listener() {
+		try {
+			server = new ServerSocket(12345);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+                Socket sock = server.accept();
+                convs.add(new GestionnaireConversation(sock));
+            }
+            catch(Exception e){
+                System.out.println("Erreur acceptation de connexion");
+                return;
+            }
+			
+			convs.removeIf(g -> (g.getSock() == null));
+		}
+		
+	}
+	
+}
