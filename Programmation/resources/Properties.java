ffeaeab40a75865fed71c6c40c7bd5a6f15c37ab
RoussPat <patrickrouss@gmail.com>
2020-01-04 12:48:18 +0100

    suppression des duplications

--- Programmation/resources/Properties.java
+++
@@ -1,9 +0,0 @@
-package resources;
-
-public class Properties {
-
-		public static String SQLiteDriver = "jdbc:sqlite:";
-		public static String BaseLocalePath = "data/storage.db";
-		public static int TCPServerSocketPort = 12345;
-		
-}
