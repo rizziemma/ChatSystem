e901b0e4836f8808d9b399f7a46dc2553eba34f4
rizziemma <rizziemma22@gmail.com>
2020-01-12 13:12:13 +0100

    path to files do dl

--- Programmation/src/resources/Properties.java
+++ Programmation/src/resources/Properties.java
@@ -3,7 +3,8 @@ package src.resources;
 public class Properties {
 
 		public static String SQLiteDriver = "jdbc:sqlite:";
-		public static String BaseLocalePath = System.getProperty("user.home")+"ChatSystem/data/storage.db";
+		public static String BaseLocalePath = System.getProperty("user.home")+"/ChatSystem/data/storage.db";
+		public static String PathToDownloadFiles = System.getProperty("user.home")+"/ChatSystem/dowloads/"; 
 		public static int TCPServerSocketPort = 12345;
 		
 }
