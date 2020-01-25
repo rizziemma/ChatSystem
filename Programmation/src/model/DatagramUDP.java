d74618c1471073fa1c6ec2698a9a480a7198c5b9
RoussPat <patrickrouss@gmail.com>
2020-01-25 10:53:43 +0100

    UDP serializable ;)

--- Programmation/src/model/DatagramUDP.java
+++ Programmation/src/model/DatagramUDP.java
@@ -1,6 +1,9 @@
 package src.model;
 
-public class DatagramUDP {
+import java.io.Serializable;
+
+public class DatagramUDP implements Serializable{
+	private static final long serialVersionUID = 1L;
 	public String type;
 	public Utilisateur payload;
 	public DatagramUDP(String s,Utilisateur u) {
