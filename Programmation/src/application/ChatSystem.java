af446fad86924e5239a8c24c167d46d025838509
rizziemma <rizziemma22@gmail.com>
2020-01-27 13:12:05 +0100

    update chemin resources

--- Programmation/src/application/ChatSystem.java
+++ Programmation/src/application/ChatSystem.java
@@ -102,7 +102,7 @@ public class ChatSystem extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			javafx.scene.Parent root = FXMLLoader.load(getClass().getResource("/resources/login.fxml"));
+			javafx.scene.Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 			Scene scene = new Scene (root);
 			primaryStage.setTitle("PaChat System");
 			primaryStage.setScene(scene);
