c5f82282291eafa689c79391c9f22e73b64e9082
RoussPat <Patrickrouss@gmail.com>
2020-01-30 10:49:04 +0100

    fix deco fermeture de conv

--- Programmation/src/affichage/ChatController.java
+++ Programmation/src/affichage/ChatController.java
@@ -96,20 +96,8 @@ public class ChatController implements Initializable, Observer {
             userListView.getItems().addAll(offline);
 
             
-            if(activeUser == null) {
-            	if(online.contains(activeUser)){
-                    textArea.setDisable(false);
-                    fileButton.setDisable(false);
-                    sendButton.setDisable(false);
-                    fileButton.setOpacity(1);
-                    sendButton.setOpacity(1);
-                } else {
-                    textArea.setDisable(true);
-                    fileButton.setDisable(true);
-                    sendButton.setDisable(true);
-                    fileButton.setOpacity(0);
-                    sendButton.setOpacity(0);
-                }
+            if(activeUser == null || !online.contains(activeUser)) {
+            	closeDiscussion();  
             }
 
         });
